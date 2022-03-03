ecf5c80c570a3e39cb2110ed35f2a6a04007e2b5
Mamba24 <akateason@qq.com>
2022-03-03 21:24:07 +0800

    Initial Commit

---
+++ PlayingSwiftUI/PlayingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingSwiftUIApp.swift
+//  PlayingSwiftUI
+//
+//  Created by teason23 on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct PlayingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
